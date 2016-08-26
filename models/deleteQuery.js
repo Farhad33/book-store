e4fd3306b1c63de9327302bc957e84b3c8c026ad
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-26 11:17:47 -0700

    delete query in progress.

---
+++ models/deleteQuery.js
@@ -0,0 +1,20 @@
+class DeleteQuery {
+  constructor( table, id ){
+    if( table === undefined  ) {
+      throw 'Table parameter is required'
+    } else if( typeof( table ) !== 'string' ) {
+      throw( 'Table parameter must be string' )
+    } else if( id === undefined ) {
+      throw( 'ID field is required' )
+    }
+
+    this.table = table
+    this.id = id
+  }
+
+  toString(){
+    return `DELETE FROM ${this.table} WHERE id=${this.id}`
+  }
+}
+
+module.exports = SelectQuery
