90741327d096e8fd4fc906c7d623e66995cae15a
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-26 10:13:09 -0700

    books pages. ahh.

--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var database = require('../database');
-//
-// import express from 'express'
-// import database, { Book } from '../database'
-// const router = express.Router()
 
-
-/* GET home page. */
 router.get('/', (req, res, next) => {
   database.Book.all()
   .then( books => res.render('books', { books } )).catch(err => res.json(err))
 });
 
 router.get('/:Id/', function(req, res) {
-  database.Book.one(req.params.Book.id)
-    .then( book => res.render( 'books/details' , { book: books } ))
+  database.Book.one(req.params.id)
+    .then( book => res.render( 'books/details' , { where: [{id}] } ))
     .catch( function( error ) { res.status( 500 ).send( error ) })
 });
 
